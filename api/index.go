6dae2d84303f1b8f38afa8ec41f6455334c3a8a7
You Young June <youngjune@planorban.com>
2024-06-24 17:13:11 -0400

    testing vercel deployment without fprintf

--- api/index.go
+++ api/index.go
@@ -2,12 +2,16 @@ package handler
 
 import (
 	"encoding/json"
-	"fmt"
+	// "fmt"
 	"net/http"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+	// fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 	response := map[string]string{"message": "Hello, World!"}
-	json.NewEncoder(w).Encode(response)
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
